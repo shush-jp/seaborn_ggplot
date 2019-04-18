b020cd7d2671c04e253afe325d3082648be573b3
SHUGO ISHIZAKA <e11039@kmjp.local>
2019-04-18 13:11:43 +0900

    ~ bar plot

--- ggplot.R
+++ ggplot.R
@@ -1,7 +1,14 @@
 library(tidyverse)
 theme_set(theme_bw())
 
-df_mpg = read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/mpg.csv')
+#df_mpg = read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/mpg.csv')
+df_mpg = read_csv('mpg.csv')
+
+#df_tips <- read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/tips.csv')
+df_tips <- read_csv('tips.csv')
+
+
+# scatter -----------------------------------------------------------------
 
 df_mpg %>% 
   ggplot(aes(x=weight, y=mpg, color=origin, shape=origin)) +
@@ -20,3 +27,17 @@ df_mpg %>%
   ggplot(aes(x=weight, y=mpg, color=origin)) +
   geom_point() +
   geom_smooth(method=lm)
+
+
+
+# bar ---------------------------------------------------------------------
+
+df_tips %>% 
+  mutate(day = fct_relevel(day, c('Thur', 'Fri', 'Sat', 'Sun')),
+         sex = fct_relevel(sex, c('Male', 'Female')),
+         smoker = fct_relevel(smoker, c('Yes', 'No'))) %>%
+  group_by(day, sex, smoker) %>% 
+  summarise(total_bill=mean(total_bill)) %>% 
+  ggplot(aes(x=day, y=total_bill, fill=sex)) +
+  geom_col(position="dodge") +
+  facet_grid(.~smoker)
